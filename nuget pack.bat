1db195960708c063caf15ebac0ac21bb81559dac
Kenneth Choe <kenneth.choe@gmail.com>
2015-07-25 20:27:06 -0500

    Nuget package supports net 4.0 and net 4.5

--- nuget pack.bat
+++ nuget pack.bat
@@ -1,4 +1,6 @@
-nuget pack ObjectTestHelpers\ObjectTestHelpers.csproj -Prop Configuration=Release
+%systemroot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /t:build /v:m /nologo /p:Configuration=Debug.Net40 ObjectTestHelpers\ObjectTestHelpers.csproj
+%systemroot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /t:build /v:m /nologo /p:Configuration=Debug.Net45 ObjectTestHelpers\ObjectTestHelpers.csproj
+nuget pack ObjectTestHelpers.nuspec
 nuget push ObjectTestHelpers.*.nupkg
 del ObjectTestHelpers.*.nupkg
 pause
